be5e5f6f1b2b1f2818ecb700c6ea9efd7c6a7fdb
user@ubuntu22.04 <mai.nutthawat@gmail.com>
2024-02-10 14:40:59 +0000

    Edit: shell install for arch linux.

--- install.sh
+++ install.sh
@@ -3,20 +3,14 @@ echo "Installation\n"
 echo "============\n\n"
 
 echo "[+] Ansible"
-echo "\n[i] Add repository"
-sudo GNUTLS_CPUID_OVERRIDE=0x1 apt-add-repository ppa:ansible/ansible
-
-echo "\n[i] Update"
-sudo GNUTLS_CPUID_OVERRIDE=0x1 apt update
 echo "\n[i] Install Ansible"
-sudo GNUTLS_CPUID_OVERRIDE=0x1 apt install ansible -y
+sudo pacman -S ansible
 echo "\n[i] Check Ansible version"
 ansible --version
 
 echo "\n\n[+] Dotfile-config"
 echo "\n[i] Download Dotfile-config"
-export GNUTLS_CPUID_OVERRIDE=0x1
-git clone -b Ubuntu22.04 --single-branch https://github.com/miracleexotic/Dotfile-config.git
+git clone -b Arch --single-branch https://github.com/miracleexotic/Dotfile-config.git
 echo "\n[i] Install via ansible-playbook"
 cd Dotfile-config/
 sudo ansible-playbook install.yaml
