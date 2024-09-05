875b40f2394acc17ef0eae6e26c53e57f852d17d
miracleexotic <mai_nutthawat@hotmail.com>
2024-09-06 00:46:51 +0700

    [EDIT] nvim - plugins{auto-session: bypass neo-tree}

--- nvim/config/nvim/lua/3mper0r/plugins/auto-session.lua
+++ nvim/config/nvim/lua/3mper0r/plugins/auto-session.lua
@@ -19,20 +19,37 @@ vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session
 telescope.load_extension("session-lens")
 
 -- enable auto-session
+local function close_neo_tree()
+	require("neo-tree.sources.manager").close_all()
+	vim.notify("closed all")
+end
+
+local function open_neo_tree()
+	vim.notify("opening neotree")
+	require("neo-tree.sources.manager").show("filesystem")
+end
+
 session.setup({
-	log_level = "info",
-	auto_session_enable_last_session = false,
-	auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
-	auto_session_enabled = true,
-	auto_save_enabled = nil,
-	auto_restore_enabled = nil,
+	-- log_level = "info",
+	-- auto_session_enable_last_session = false,
+	-- auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+	-- auto_session_enabled = true,
+	auto_session_create_enabled = true,
+	auto_save_enabled = true,
+	auto_restore_enabled = true,
 	auto_session_suppress_dirs = nil,
-	auto_session_use_git_branch = nil,
+	auto_session_use_git_branch = true,
 	-- the configs below are lua only
-	bypass_session_save_file_types = nil,
+	bypass_session_save_file_types = { "neo-tree" },
+	pre_save_cmds = {
+		close_neo_tree,
+	},
+	post_restore_cmds = {
+		open_neo_tree,
+	},
 	session_lens = {
 		-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-		buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+		buftypes_to_ignore = { "neo-tree" }, -- list of buffer types what should not be deleted from current session
 		load_on_setup = true,
 		theme_conf = { border = true },
 		previewer = false,
