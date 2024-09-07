05fc3ad8d3ab853c53aeb9b22f6fb3c05ae90c1b
miracleexotic <mai_nutthawat@hotmail.com>
2024-09-08 04:44:36 +0700

    [EDIT] nvim - colorscheme{ignore diagnostic}

--- nvim/config/nvim/lua/3mper0r/core/colorscheme.lua
+++ nvim/config/nvim/lua/3mper0r/core/colorscheme.lua
@@ -1,11 +1,13 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
+---@diagnostic disable-next-line: param-type-mismatch
 local status, _ = pcall(vim.cmd, "colorscheme dracula-soft")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
 end
 
+---@diagnostic disable-next-line: param-type-mismatch, redefined-local
 local status, _ = pcall(vim.cmd, "highlight CursorLine ctermbg=NONE guibg=#444444")
 if not status then
 	print("CursorLine cannot apply highlight")
