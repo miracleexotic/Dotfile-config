07c233a462caad9b77539ce6409b2903b2f4a607
miracleexotic <mai_nutthawat@hotmail.com>
2024-09-08 04:45:16 +0700

    [ADD] nvim - keymaps{new keymaps}

--- nvim/config/nvim/lua/3mper0r/core/keymaps.lua
+++ nvim/config/nvim/lua/3mper0r/core/keymaps.lua
@@ -7,42 +7,65 @@ local keymap = vim.keymap -- for conciseness
 -- General Keymaps
 ---------------------
 
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>")
-
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "[N]o [H]ighlight" })
 
 -- delete single character without copying into register
-keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x', { desc = "[X]elete" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>wsv", "<C-w>v", { desc = "[W]orkspace [S]plit [V]ertically" }) -- split window vertically
+keymap.set("n", "<leader>wsv", "<C-w>s", { desc = "[W]orkspace [S]plit [H]orizontally" }) -- split window horizontally
+keymap.set("n", "<leader>wse", "<C-w>=", { desc = "[W]orkspace [S]plit [E]qual" }) -- make split windows equal width & height
+keymap.set("n", "<leader>wsx", ":close<CR>", { desc = "[W]orkspace [S]plit [C]lose" }) -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+keymap.set("n", "<leader>wto", ":tabnew<CR>", { desc = "[W]orkspace [T]ab [O]pen" }) -- open new tab
+keymap.set("n", "<leader>wtx", ":tabclose<CR>", { desc = "[W]orkspace [T]ab [C]lose" }) -- close current tab
+keymap.set("n", "<leader>wtn", ":tabn<CR>", { desc = "[W]orkspace [T]ab [N]ext" }) --  go to next tab
+keymap.set("n", "<leader>wtp", ":tabp<CR>", { desc = "[W]orkspace [T]ab [P]revious" }) --  go to previous tab
 
 ----------------------
 -- Plugin Keybinds
 ----------------------
 
 -- neo-tree
-keymap.set("n", "<leader>e", ":Neotree reveal<cr>") -- toggle file explorer
+keymap.set("n", "<leader>e", ":Neotree reveal<cr>", { desc = "Neotree [E]xplorer" }) -- toggle file explorer
 
 -- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+-- See `:help telescope.builtin`
+keymap.set("n", "<leader>sh", "<cmd>Telescope help_tags<cr>", { desc = "[S]earch [H]elp" })
+keymap.set("n", "<leader>sk", "<cmd>Telescope keymaps<cr>", { desc = "[S]earch [K]eymaps" })
+keymap.set("n", "<leader>sf", "<cmd>Telescope find_files<cr>", { desc = "[S]earch [F]iles" })
+keymap.set("n", "<leader>ss", "<cmd>Telescope builtin<cr>", { desc = "[S]earch [S]elect Telescope" })
+keymap.set("n", "<leader>sw", "<cmd>Telescope grep_string<cr>", { desc = "[S]earch current [W]ord" })
+keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", { desc = "[S]earch by [G]rep" })
+keymap.set("n", "<leader>sd", "<cmd>Telescope diagnostics<cr>", { desc = "[S]earch [D]iagnostics" })
+keymap.set("n", "<leader>sr", "<cmd>Telescope resume<cr>", { desc = "[S]earch [R]esume" })
+keymap.set("n", "<leader>s.", "<cmd>Telescope oldfiles<cr>", { desc = '[S]earch Recent Files ("." for repeat)' })
+keymap.set("n", "<leader><leader>", "<cmd>Telescope buffers<cr>", { desc = "[ ] Find existing buffers" })
 
 -- restart lsp server (not on youtube nvim video)
-keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+keymap.set("n", "<leader>lrs", ":LspRestart<CR>", { desc = "[L]SP [R]e[S]tart" }) -- mapping to restart lsp if necessary
 
 -- python env w/ swenv
-keymap.set("n", "<leader>pe", "<cmd>lua require('swenv.api').pick_venv()<cr>")
+keymap.set("n", "<leader>sp", "<cmd>lua require('swenv.api').pick_venv()<cr>", { desc = "[S]earch [P]ython env" })
+
+-- auto-session
+vim.keymap.set("n", "<leader>sa", require("auto-session.session-lens").search_session, {
+	desc = "[S]earch [A]uto Session",
+	noremap = true,
+})
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
