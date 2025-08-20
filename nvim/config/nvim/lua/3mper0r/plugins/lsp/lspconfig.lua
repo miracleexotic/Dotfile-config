9da208d71f810895d3518aa059f17c36acac5a01
miracleexotic <mai_nutthawat@hotmail.com>
2025-08-21 00:02:38 +0700

    Edit: Config new LSP keybinds

--- nvim/config/nvim/lua/3mper0r/plugins/lsp/lspconfig.lua
+++ nvim/config/nvim/lua/3mper0r/plugins/lsp/lspconfig.lua
@@ -10,67 +10,70 @@ if not cmp_nvim_lsp_status then
 	return
 end
 
----@diagnostic disable-next-line: deprecated
-table.unpack = table.unpack or unpack -- 5.1 compatibility
+-- NOTE: LSP Keybinds
 
-local keymap = vim.keymap -- for conciseness
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Buffer local mappings
+		-- Check `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf, silent = true }
 
--- enable keybinds only for when lsp server available
-local on_attach = function(client, bufnr)
-	-- keybind options
-	local opts = { noremap = true, silent = true, buffer = bufnr }
+		-- keymaps
+		opts.desc = "Show LSP [F]inder"
+		vim.keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts) -- show definition, references
 
-	-- set keybinds
-	keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", { table.unpack(opts), desc = "[G]oto [F]inder" }) -- show definition, references
-	keymap.set(
-		"n",
-		"gD",
-		"<cmd>lua vim.lsp.buf.declaration()<CR>",
-		{ table.unpack(opts), desc = "[G]oto [D]eclaration" }
-	) -- got to declaration
-	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { table.unpack(opts), desc = "[G]oto [d]efinition" }) -- see definition and make edits in window
-	keymap.set(
-		"n",
-		"gi",
-		"<cmd>lua vim.lsp.buf.implementation()<CR>",
-		{ table.unpack(opts), desc = "[G]oto [I]mplementation" }
-	) -- go to implementation
-	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { table.unpack(opts), desc = "[C]ode [A]ctions" }) -- see available code actions
-	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { table.unpack(opts), desc = "[R]e[N]ame" }) -- smart rename
-	keymap.set(
-		"n",
-		"<leader>ld",
-		"<cmd>Lspsaga show_line_diagnostics<CR>",
-		{ table.unpack(opts), desc = "[L]ine [D]iagnostic" }
-	) -- show  diagnostics for line
-	keymap.set(
-		"n",
-		"<leader>cd",
-		"<cmd>Lspsaga show_cursor_diagnostics<CR>",
-		{ table.unpack(opts), desc = "[C]ursor [D]iagnostic" }
-	) -- show diagnostics for cursor
-	keymap.set(
-		"n",
-		"[d",
-		"<cmd>Lspsaga diagnostic_jump_prev<CR>",
-		{ table.unpack(opts), desc = "[P]revious [D]iagnostic" }
-	) -- jump to previous diagnostic in buffer
-	keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { table.unpack(opts), desc = "[N]ext [D]iagnostic" }) -- jump to next diagnostic in buffer
-	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { table.unpack(opts), desc = "[K]over" }) -- show documentation for what is under cursor
-	keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { table.unpack(opts), desc = "[O]utline" }) -- see outline on right hand side
+		opts.desc = "Goto [D]eclaration"
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-	-- typescript specific keymaps (e.g. rename file and update imports)
-	if client.name == "vtsls" then
-		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
-		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
-	end
-end
+		opts.desc = "Show LSP [d]efinitions"
+		vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- show lsp definitions
+
+		opts.desc = "Show LSP [i]mplementations"
+		vim.keymap.set("n", "gi", "<cmd>Lspsaga goto_definition<CR>", opts) -- show lsp implementations
+
+		opts.desc = "[G]oto [T]ype definitions"
+		vim.keymap.set("n", "gT", "<cmd>Lspsaga goto_type_definition<CR>", opts) -- show lsp type definitions
+
+		opts.desc = "Show LSP [t]ype definitions"
+		vim.keymap.set("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>", opts) -- show lsp type definitions
+
+		opts.desc = "Show LSP [c]ode [a]ctions"
+		vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions, in visual mode will apply to selection
+
+		opts.desc = "Smart [r]e[n]ame"
+		vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+
+		opts.desc = "Show [b]uffer [d]iagnostics"
+		vim.keymap.set("n", "<leader>bd", "<cmd>Lspsaga show_buf_diagnostics<CR>", opts) -- show  diagnostics for file
+
+		opts.desc = "Show [l]ine [d]iagnostics"
+		vim.keymap.set("n", "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show diagnostics for line
+
+		opts.desc = "Show [c]ursor [d]iagnostics"
+		vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for line
+
+		opts.desc = "Show do[K]umentation"
+		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+
+		opts.desc = "Show [o]utline"
+		vim.keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<CR>", opts) -- show documentation for what is under cursor
+
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+	end,
+})
+
+-- NOTE : Moved all this to Mason including local variables
+-- used to enable autocompletion (assign to every lsp server config)
+-- local capabilities = cmp_nvim_lsp.default_capabilities()
+-- Change the Diagnostic symbols in the sign column (gutter)
+
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
 vim.diagnostic.config({
 	signs = {
 		text = {
@@ -79,55 +82,49 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.HINT] = "󰠠 ",
 			[vim.diagnostic.severity.INFO] = " ",
 		},
-		numhl = {
-			[vim.diagnostic.severity.ERROR] = "",
-			[vim.diagnostic.severity.WARN] = "",
-			[vim.diagnostic.severity.HINT] = "",
-			[vim.diagnostic.severity.INFO] = "",
-		},
+		virtual_text = true, -- Specify Enable virtual text for diagnostics
+		underline = true, -- Specify Underline diagnostics
+		update_in_insert = false, -- Keep diagnostics active in insert mode
 	},
 })
 
 -- configure html server
 lspconfig["html"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 })
 
 -- configure typescript server with plugin
 lspconfig["vtsls"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 })
 
 -- configure css server
 lspconfig["cssls"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 })
 
 -- configure tailwindcss server
 lspconfig["tailwindcss"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 })
 
 -- configure emmet language server
 lspconfig["emmet_ls"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
 })
 
 -- configure lua server (with special settings)
 lspconfig["lua_ls"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 	settings = { -- custom settings for lua
 		Lua = {
 			-- make the language server recognize "vim" global
 			diagnostics = {
 				globals = { "vim" },
+			},
+			completion = {
+				callSnippet = "Replace",
 			},
 			workspace = {
 				-- make language server aware of runtime files
@@ -143,7 +140,6 @@ lspconfig["lua_ls"].setup({
 -- configure python server
 lspconfig["pyright"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 	filetypes = { "python" },
 	settings = {
 		pyright = {
@@ -170,6 +166,5 @@ lspconfig["pyright"].setup({
 -- configure php server
 lspconfig["intelephense"].setup({
 	capabilities = capabilities,
-	on_attach = on_attach,
 	filetypes = { "php" },
 })
